4d45b01a09266470b665e9325380477a409304d9
kimmi <kimmikumarisinha@gmail.com>
2022-11-22 00:55:30 +0530

    completed model

--- src/index.js
+++ src/index.js
@@ -9,12 +9,11 @@ dotenv.config();
 const app = express();
 
 app.use(multer().any());
-console.log(process.env.PORT);
-// mongoose.connect(process.env.DB_URL).then( _ => console.log('DB connect')).catch((err) => console.log(err));
+mongoose.connect(process.env.DB_URL).then( _ => console.log('DB connect')).catch((err) => console.log(err));
 
 app.use('/',rout)
 
 
-app.listen(process.env.PORT , ()=>{
+app.listen(process.env.PORT || 2360 , ()=>{
     console.log('server is working');
 })
