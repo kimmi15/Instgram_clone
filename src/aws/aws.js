d8fe6a7ffa1f4d6fa543ac40b74f7d51f66689c8
kimmi <kimmikumarisinha@gmail.com>
2022-11-27 20:32:49 +0530

    added all post api

--- src/aws/aws.js
+++ src/aws/aws.js
@@ -1,13 +1,13 @@
 const aws= require("aws-sdk")
 aws.config.update({
-    accessKeyId: "AKIAY3L35MCRZNIRGT6N",
-    secretAccessKey: "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",
-    region: "ap-south-1"
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    region: process.env.REGION
 });
 
 let uploadFile= async (file) =>{
    return new Promise( function(resolve, reject) {
-    let s3= new aws.S3({apiVersion: '2006-03-01'}); 
+    let s3 = new aws.S3({apiVersion: '2006-03-01'}); 
 
     var uploadParams= {
         ACL: "public-read",
